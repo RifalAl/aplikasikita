177fabc714861a56eba554cc1a7d1c447075d0ae
Ahmad Rifaldi <aldi4rifaldi@gmail.com>
2020-07-06 14:11:01 +0800

    commit pertama

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+function HelloWorld(){
+   return (
+      <p style={{color:'tomato'}}>
+         Hello World!!
+      </p>
+   )
+}
+function ButtonWelcome(){
+   return (
+      <button onClick={() => alert('Selamat Datang')}>
+         coba klik saya!
+      </button>
+   )
+}
+function App(){
+   return <div>
+      <HelloWorld/>
+      <ButtonWelcome/>
+   </div>
 }
 
 export default App;
